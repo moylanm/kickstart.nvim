54a784dfdac79822cec9b4a97cd80f2e05ebea27
Myles Moylan <moylanm@fastmail.com>
2023-12-13 15:37:22 -0800

    Add java to server and treesitter lists

--- init.lua
+++ init.lua
@@ -360,7 +360,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'dart', 'lua', 'python', 'ruby', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'java', 'dart', 'lua', 'python', 'ruby', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'json'},
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -475,6 +475,7 @@ local servers = {
   solargraph = {},
   bashls = {},
   gopls = {},
+  jdtls = {},
   rust_analyzer = {},
   taplo = {},
   eslint = {},
