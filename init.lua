aebb1aa222915e142da5c462280f2ac1ad3126f2
Myles Moylan <moylanm@fastmail.com>
2025-09-09 07:46:01 -0700

    Diagnostic keymap, `emmet-ls`, and transparent background

--- init.lua
+++ init.lua
@@ -175,6 +175,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -248,7 +249,6 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-  'microsoft/pyright',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -679,6 +679,9 @@ require('lazy').setup({
         eslint = {},
         sqls = {},
         html = {},
+        emmet_ls = {
+          filetypes = { 'html', 'eruby' },
+        },
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -894,6 +897,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('kanagawa').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
